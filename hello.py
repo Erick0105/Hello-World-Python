35e4c15ae99acf0f3421a36a65a6ffe03c009879
Erick0105 <erickspot01@gmail.com>
2024-05-19 18:36:16 -0300

    forma reduzida

--- hello.py
+++ hello.py
@@ -7,36 +7,15 @@ lista = list(string.printable)
 
 for i in range(5):
     lista.pop(-1)
+msgfinal = ""
+letra = ""
 
-msg = ""
-while msg != "Hello World":
-    letra = random.choice(lista)
-    print(msg + letra)
-    if letra == "H" and msg == "":
-        msg = msg + letra
-    elif letra == "e" and msg == "H":
-        msg = msg + letra
-    elif letra == "l" and msg == "He":
-        msg = msg + letra
-    elif letra == "l" and msg == "Hel":
-        msg = msg + letra
-    elif letra == "o" and msg == "Hell":
-        msg = msg + letra
-    elif letra == " " and msg == "Hello":
-        msg = msg + letra
-    elif letra == "W" and msg == "Hello ":
-        msg = msg + letra
-    elif letra == "o" and msg == "Hello W":
-        msg = msg + letra
-    elif letra == "r" and msg == "Hello Wo":
-        msg = msg + letra
-    elif letra == "l" and msg == "Hello Wor":
-        msg = msg + letra
-    elif letra == "d" and msg == "Hello Worl":
-        msg = msg + letra
-    time.sleep(0.03)
-    
-
+for i in "Hello World":
+    while letra != i:
+        letra = random.choice(lista)
+        print(msgfinal + letra)
+        time.sleep(0.03)
+    msgfinal = msgfinal + letra
 os.system('cls')
     
-print(msg + " (;")
+print(msgfinal + " (;")
